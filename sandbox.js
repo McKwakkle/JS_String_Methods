ab4be2b4075f27e58d43ec6aae493e0b01c1d67b
Kellan Mc Naughton <kellanmcn@gmail.com>
2023-02-27 10:33:17 +0200

    Added HTML and JS
    Just doing very basic string methods

---
+++ sandbox.js
@@ -0,0 +1,17 @@
+// common string methods
+
+let email = 'kellanmcn@gmail.com';
+
+// let result = email.lastIndexOf('n');
+
+// let result = email.slice(0, 6)
+
+// let result = email.substring(0, 10);
+
+// replace will replace the first letter
+
+// let result = email.replace('m', 'z');
+
+// let result = email.replace('l', 'j');
+
+console.log(result);
